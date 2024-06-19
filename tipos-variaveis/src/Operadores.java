e7f5b725231d1039506f2c012818990ff5432273
KauaJenior <kauafe54@gmail.com>
2024-06-19 13:42:42 -0300

    novas alterações

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,15 +1,34 @@
 public class Operadores {
 
     public static void main(String[] args) {
-         int numero = 5;
 
-         System.out.println(++numero)   ;
-       
+        int a = 5;
+        int b = 6;
 
-     
+        boolean simNao = a == b;
 
-        
+        System.out.println("n1 é igual a n2? " + simNao);
+
+        simNao = a != b;
+
+        System.out.println("n1 é diferente a n2? " + simNao);
+
+        simNao = a > b;
+
+        System.out.println("a é maior que b?" + simNao);
+
+        boolean condicao1 = true;
+
+        boolean condicao2 = false;
+
+        if (condicao1 && condicao2){
+            System.out.print("Os dois valores precisam ser verdadeiros");;
+        }
+        // Se condicao1 OU condicao2 for verdadeira, executar código.
+        if (condicao1 || condicao2){
+            System.out.print("Um dos valores precisa ser verdadeiro");
 
     }
-    
+
+}
 }
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        int numeroCurto2 = numeroNormal;
+
+        final int NUMERO1 = 5;
+
+        final int NUMERO2 = 10;
+
+        int soma = NUMERO1 + NUMERO2;
+
+        System.out.println(soma);   
     }
 }
